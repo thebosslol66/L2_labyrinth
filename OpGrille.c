ba7b7333a8663a790224a8e40679f726dba65a55
Jojodu7425 <83352140+Jojodu7425@users.noreply.github.com>
2021-12-02 15:01:32 +0100

    Add files via upload
    other

--- OpGrille.c
+++ OpGrille.c
@@ -19,7 +19,6 @@ void board_create(struct Board *self, int width, int height, int x, int y, int x
 
 
 
-
 char * array_to_line(int w, int h) {
     int size = w * h;
     char *line = calloc(size, sizeof(size_t));
@@ -30,106 +29,119 @@ char * array_to_line(int w, int h) {
     return line;
 }
 
-char * pos_and_char_to_line(struct Board *self, int x, int y, char *status, char *line, int size) {
-    int i = 0;
-    if (x == h-1 || x == 0 || y == w-1 || y == 0){
-        if (x == h-1)
+ void board_update(struct Board *self, int x, int y, char *status) {
+    for (int r = 0; r < self->width; r++)
+    {
+        for (int c = 0; r < self->height; c++)
         {
-            if (y == 0)
-            {
-                line[(x-1)+(y)*w] = status[1];
-                line[(x-1)+(y)*w] = status[2];
-                line[(x-1)+(y)*w] = status[4];
-            }else if (y == w-1)
-            {
-                line[(x-1)+(y-1)*w] = status[0];
-                line[(x-1)+(y)*w] = status[1]
-    int heuristique;;
-                line[(x)+(y-1)*w] = status[3];
-            }else
-            {
-                line[(x-1)+(y-1)*w] = status[0];
-                line[(x-1)+(y)*w] = status[1];
-                line[(x-1)+(y+1)*w] = status[2];
-                line[(x)+(y-1)*w] = status[3];
-                line[(x)+(y+1)*w] = status[4];
-            }
-        }else
-        if (x == 0)
-        {
-            if (y == 0)
-            {
-                line[(x)+(y+1)*w] = status[4];
-                line[(x+1)+(y+1)*w] = status[6];
-                line[(x+1)+(y)*w] = status[7];
-            }else if (y == w-1)
-            {
-                line[(x)+(y-1)*w] = status[3];
-                line[(x+1)+(y-1)*w] = status[5];
-                line[(x+1)+(y)*w] = status[6];
-            }else
-            {
-                line[(x)+(y-1)*w] = status[3];
-                line[(x)+(y+1)*w] = status[4];
-                line[(x-1)+(y-1)*w] = status[5];
-                line[(x-1)+(y)*w] = status[6];
-                line[(x-1)+(y+1)*w] = status[7];
-            }
-        }else
-        if (y == w-1)
-        {
-            line[(x-1)+(y-1)*w] = status[0];
-            line[(x-1)+(y)*w] = status[1];
-            line[(x)+(y-1)*w] = status[3];
-            line[(x+1)+(y-1)*w] = status[5];
-            line[(x+1)+(y)*w] = status[6];
-        }else
-        {
-            line[(x-1)+(y)*w] = status[1];
-            line[(x-1)+(y+1)*w] = status[2];
-            line[(x)+(y+1)*w] = status[4];
-            line[(x+1)+(y)*w] = status[6];
-            line[(x+1)+(y+1)*w] = status[7];
-        }
-
-    }else{
-        for (i = 0; i < 8; i++)
-        {
-            switch(i){
-                case 0 :
-                    line[(x-1)+(y-1)*w] = status[i];
-                break;
-
-                case 1 :
-                    line[(x-1)+(y)*w] = status[i];
-                break;
-
-                case 2 :
-                    line[(x-1)+(y+1)*w] = status[i];
-                break;
-
-                case 3 :
-                    line[(x)+(y-1)*w] = status[i];
-                break;
-
-                case 4 :
-                    line[(x)+(y+1)*w] = status[i];
-                break;
-
-                case 5 :
-                    line[(x+1)+(y-1)*w] = status[i];
-                break;
-
-                case 6 :
-                    line[(x+1)+(y)*w] = status[i];
-                break;
-
-                case 7 :
-                    line[(x+1)+(y+1)*w] = status[i];
-                break;
-            }
+            int imin = c > 0? c-1 : c;
+            int imax = c < self->height-1? c+1 : c;
+            int jmin = r > 0? r-1 : r;
+            int jmax = r < self->height-1? r+1 : r;
         }
     }
+    
+    
+    
+    
+    // int i = 0;
+    // if (x == h-1 || x == 0 || y == w-1 || y == 0){
+    //     if (x == h-1)
+    //     {
+    //         if (y == 0)
+    //         {
+    //             line[(x-1)+(y)*w] = status[1];
+    //             line[(x-1)+(y)*w] = status[2];
+    //             line[(x-1)+(y)*w] = status[4];
+    //         }else if (y == w-1)
+    //         {
+    //             line[(x-1)+(y-1)*w] = status[0];
+    //             line[(x-1)+(y)*w] = status[1]
+    //             line[(x)+(y-1)*w] = status[3];
+    //         }else
+    //         {
+    //             line[(x-1)+(y-1)*w] = status[0];.
+    //             line[(x-1)+(y)*w] = status[1];
+    //             line[(x-1)+(y+1)*w] = status[2];
+    //             line[(x)+(y-1)*w] = status[3];
+    //             line[(x)+(y+1)*w] = status[4];
+    //         }
+    //     }else
+    //     if (x == 0)
+    //     {
+    //         if (y == 0)
+    //         {
+    //             line[(x)+(y+1)*w] = status[4];
+    //             line[(x+1)+(y+1)*w] = status[6];
+    //             line[(x+1)+(y)*w] = status[7];
+    //         }else if (y == w-1)
+    //         {
+    //             line[(x)+(y-1)*w] = status[3];
+    //             line[(x+1)+(y-1)*w] = status[5];
+    //             line[(x+1)+(y)*w] = status[6];
+    //         }else
+    //         {
+    //             line[(x)+(y-1)*w] = status[3];
+    //             line[(x)+(y+1)*w] = status[4];
+    //             line[(x-1)+(y-1)*w] = status[5];
+    //             line[(x-1)+(y)*w] = status[6];
+    //             line[(x-1)+(y+1)*w] = status[7];
+    //         }
+    //     }else
+    //     if (y == w-1)
+    //     {
+    //         line[(x-1)+(y-1)*w] = status[0];
+    //         line[(x-1)+(y)*w] = status[1];
+    //         line[(x)+(y-1)*w] = status[3];
+    //         line[(x+1)+(y-1)*w] = status[5];
+    //         line[(x+1)+(y)*w] = status[6];
+    //     }else
+    //     {
+    //         line[(x-1)+(y)*w] = status[1];
+    //         line[(x-1)+(y+1)*w] = status[2];
+    //         line[(x)+(y+1)*w] = status[4];
+    //         line[(x+1)+(y)*w] = status[6];
+    //         line[(x+1)+(y+1)*w] = status[7];
+    //     }
+
+    // }else{
+    //     for (i = 0; i < 8; i++)
+    //     {
+    //         switch(i){
+    //             case 0 :
+    //                 line[(x-1)+(y-1)*w] = status[i];
+    //             break;
+
+    //             case 1 :
+    //                 line[(x-1)+(y)*w] = status[i];
+    //             break;
+
+    //             case 2 :
+    //                 line[(x-1)+(y+1)*w] = status[i];
+    //             break;
+
+    //             case 3 :
+    //                 line[(x)+(y-1)*w] = status[i];
+    //             break;
+
+    //             case 4 :
+    //                 line[(x)+(y+1)*w] = status[i];
+    //             break;
+
+    //             case 5 :
+    //                 line[(x+1)+(y-1)*w] = status[i];
+    //             break;
+
+    //             case 6 :
+    //                 line[(x+1)+(y)*w] = status[i];
+    //             break;
+
+    //             case 7 :
+    //                 line[(x+1)+(y+1)*w] = status[i];
+    //             break;
+    //         }
+    //     }
+    // }
 }
 
 // || x == h-1 && y == w-1 ||x == 0 && y == 0 || x == 0 && y == w-1 
