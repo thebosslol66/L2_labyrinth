7426019041bdebd64def0cd8dbeb6b730b9e0d21
Alphee GROSDIDIER <alphee.grosdidier@gmail.com>
2022-01-07 17:51:33 +0100

    pfiou

--- autoPlayer.c
+++ autoPlayer.c
@@ -15,23 +15,24 @@ int main() {
     struct Board board;
 
     char buf[BUFSIZE];
+    char * t;
     // get the width
-    fgets(buf, BUFSIZE, stdin);
+    t = fgets(buf, BUFSIZE, stdin);
     int width = atoi(buf);
     // get the height
-    fgets(buf, BUFSIZE, stdin);
+    t = fgets(buf, BUFSIZE, stdin);
     int height = atoi(buf);
     // get the initial x
-    fgets(buf, BUFSIZE, stdin);
+    t = fgets(buf, BUFSIZE, stdin);
     int x = atoi(buf);
     // get the initial y
-    fgets(buf, BUFSIZE, stdin);
+    t = fgets(buf, BUFSIZE, stdin);
     int y = atoi(buf);
     // get the treasure x
-    fgets(buf, BUFSIZE, stdin);
+    t = fgets(buf, BUFSIZE, stdin);
     int xt = atoi(buf);
     // get the treasure y
-    fgets(buf, BUFSIZE, stdin);
+    t = fgets(buf, BUFSIZE, stdin);
     int yt = atoi(buf);
 
 
@@ -39,25 +40,21 @@ int main() {
     // initialize the state of the game
     board_create(&board, width, height, x, y, xt, yt);
 
-
-
-    //fgets(buf, BUFSIZE, stderr);
-    //fprintf(stderr, "Flush : %s\n", buf);
-
     for (;;) {
     // get the adjacent cells
-    fgets(buf, BUFSIZE, stdin);
+    t = fgets(buf, BUFSIZE, stdin);
     // update the state of the game
     // send the new direction
     board_update(&board, buf);
 
     puts(think(&board)); // or "SOUTH" or "EAST" or "WEST"
     // get the result
-    fgets(buf, BUFSIZE, stdin);
+    t = fgets(buf, BUFSIZE, stdin);
     if (strcmp(buf, "END\n") == 0) {
     break;
     }
     }
     board_destroy(&board);
+    free(t);
     return 0;
 }
